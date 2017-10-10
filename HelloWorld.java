3e16b1dbfc6d684f94c0f45da2984edd6bbd6668
shirakawas <shirakawa-shinichi-bg@ynu.ac.jp>
2017-10-11 01:37:52 +0900

    add files

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//HelloWorld.java
+// "//"以降はコメントです
+import java.lang.*; // java.lang.* は default で import されるので省略可
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
